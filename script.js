c52f0deacb406c576a82384f694231e440849c5f
AYOOLA IYANUOLUWA ANU <Ayoola Iyanuoluwa>
2024-05-05 16:24:46 +0100

    go

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 function StartTime() {
     var date = new Date
     document.getElementById('clock').innerHTML= date.toLocaleTimeString();
+    document.getElementById('date').innerHTML=date.toLocaleDateString();
     setTimeout(function() {StartTime()}, 1000)
     
 }
